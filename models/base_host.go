60440e219d4e24764da1ca56345be32cdbe54d39
lana <iikmaulana369@gmail.com>
2023-05-11 12:05:48 +0700

    Add payload host

--- models/base_host.go
+++ models/base_host.go
@@ -1,23 +1,35 @@
 package models
 
 type HostCreateRequest struct {
-	Name string `json:"name"`
-	Url  string `json:"url"`
-	Port string `json:"port"`
+	Name         string `json:"name"`
+	Url          string `json:"url"`
+	Port         string `json:"port"`
+	HostUsername string `json:"host_username"`
+	HostPassword string `json:"host_password"`
+	HostClientId string `json:"host_client_id"`
+	HostType     string `json:"host_type"`
 }
 
 type HostUpdateRequest struct {
-	Id   string `json:"id" db:"id"`
-	Name string `json:"name" db:"name"`
-	Url  string `json:"url" db:"url"`
-	Port string `json:"port" db:"port"`
+	Id           string `json:"id" db:"id"`
+	Name         string `json:"name" db:"name"`
+	Url          string `json:"url" db:"url"`
+	Port         string `json:"port" db:"port"`
+	HostUsername string `json:"host_username" db:"host_username"`
+	HostPassword string `json:"host_password" db:"host_password"`
+	HostClientId string `json:"host_client_id" db:"host_client_id"`
+	HostType     string `json:"host_type" db:"host_type"`
 }
 
 type HostResult struct {
-	Id   string `json:"id" db:"id"`
-	Name string `json:"name" db:"name"`
-	Url  string `json:"url" db:"url"`
-	Port string `json:"port" db:"port"`
+	Id           string `json:"id" db:"id"`
+	Name         string `json:"name" db:"name"`
+	Url          string `json:"url" db:"url"`
+	Port         string `json:"port" db:"port"`
+	HostUsername string `json:"host_username" db:"host_username"`
+	HostPassword string `json:"host_password" db:"host_password"`
+	HostClientId string `json:"host_client_id" db:"host_client_id"`
+	HostType     string `json:"host_type" db:"host_type"`
 }
 
 type HostListResult struct {
