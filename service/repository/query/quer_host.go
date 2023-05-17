806937f35120958ce3f22667e3e693eaeb42e1aa
lana <iikmaulana369@gmail.com>
2023-05-17 16:19:06 +0700

    Add payload topic query

--- service/repository/query/quer_host.go
+++ service/repository/query/quer_host.go
@@ -1,7 +1,7 @@
 package query
 
 const (
-	QueryHostInsert = `INSERT INTO db_terminal.host (id,name,url,port,host_username,host_password,host_client_id,host_type,topic) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) returning id`
+	QueryHostInsert = `INSERT INTO db_terminal.host (id,name,url,port,host_username,host_password,host_client_id,host_type,topic) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) returning id`
 	QueryHostList   = `SELECT id,name,url,port,host_username,host_password,host_client_id,host_type,topic FROM db_terminal.host`
 	QueryHostView   = `SELECT id,name,url,port,host_username,host_password,host_client_id,host_type,topic FROM db_terminal.host WHERE id = $1`
 	QueryHostDelete = `DELETE FROM db_terminal.host WHERE id = $1`
